48e3f654450ada0931fb9d2ad525a757514f858a
Talha Dar <tdtalhadar@gmail.com>
2020-05-09 14:34:43 +0500

    indentation changes.

--- lab9/main.cpp
+++ lab9/main.cpp
@@ -6,49 +6,49 @@ using namespace std;
 
 int main()
 {
-    char filename[] = {"processes.txt"};
-    int count = 0, _id = 0, _lines = 0;
+  char filename[] = {"processes.txt"};
+  int count = 0, _id = 0, _lines = 0;
 
-    //open file, and iterate through it to find number of lines.
-    ifstream fin(filename);
-    if (!fin.is_open())
+  //open file, and iterate through it to find number of lines.
+  ifstream fin(filename);
+  if (!fin.is_open())
     {
-        cout << "\nFile not found.";
-        return 0;
+      cout << "\nFile not found.";
+      return 0;
     }
 
-    //iterate filr marker
-    while (!fin.eof())
+  //iterate filr marker
+  while (!fin.eof())
     {
-        fin.ignore('\n');
-        count++;
+      fin.ignore('\n');
+      count++;
     }
 
-    //create dynamic memory
-    Process* proc_list[count];
+  //create dynamic memory
+  Process* proc_list[count];
 
-    //repopen file to reopen file to reset file marker
-    fin.close();
-    fin.open(filename);
+  //repopen file to reopen file to reset file marker
+  fin.close();
+  fin.open(filename);
 
-    //read elements from file and instantiate objects on proc_list
-    for (int i = 0; i < count; i++)
+  //read elements from file and instantiate objects on proc_list
+  for (int i = 0; i < count; i++)
     {
-        fin >> _id >> _lines;
-        proc_list[i] = new Process(_id, _lines);
+      fin >> _id >> _lines;
+      proc_list[i] = new Process(_id, _lines);
     }
-    //close file
-    fin.close();
+  //close file
+  fin.close();
     
-    //print static members
-    Process::display();
+  //print static members
+  Process::display();
 
-    //print non static members
-    print(proc_list, count);
+  //print non static members
+  print(proc_list, count);
 
-    //delete memory
-    for(int i=0; i<count;i++){
-	delete proc_list[i];
-    }
-    return 0;
+  //delete memory
+  for(int i=0; i<count;i++){
+    delete proc_list[i];
+  }
+  return 0;
 }
