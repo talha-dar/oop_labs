2b8b4b44d39d64cb35feae49fa88e527393425e2
Talha Dar <tdtalhadar@gmail.com>
2020-05-09 14:27:55 +0500

    indentation fixes.

--- lab9/Process.cpp
+++ lab9/Process.cpp
@@ -3,81 +3,81 @@
 //default parametrized constructor
 Process::Process(const int _id, const int _lines)
 {
-    //proceed to set/mutate attributes only if proc id is valid
-    if (_id > 0)
-	{
-	    id = _id;
-	    //update attributes related to proc lines if arg if valid
-	    if (_lines > 0)
-		{
-		    lines = _lines;
-
-		    totalLinesOfCode += _lines;
-
-		    if (_lines > maxLinesOfCode)
-			{
-			    maxLinesOfCode = _lines;
-			}
-		    if (_lines < minLinesOfCode || count  == 0)
-			{
-			    minLinesOfCode = _lines;
-			}
-		}
-//update count of processes
-	    count++;
-//update the average
-	    avgLinesPerProcess = totalLinesOfCode / count;
-    }
-    else
+  //proceed to set/mutate attributes only if proc id is valid
+  if (_id > 0)
     {
-        id = 0, lines = 0;
+      id = _id;
+      //update attributes related to proc lines if arg if valid
+      if (_lines > 0)
+	{
+	  lines = _lines;
+
+	  totalLinesOfCode += _lines;
+
+	  if (_lines > maxLinesOfCode)
+	    {
+	      maxLinesOfCode = _lines;
+	    }
+	  if (_lines < minLinesOfCode || count  == 0)
+	    {
+	      minLinesOfCode = _lines;
+	    }
+	}
+      //update count of processes
+      count++;
+      //update the average
+      avgLinesPerProcess = totalLinesOfCode / count;
+    }
+  else
+    {
+      id = 0, lines = 0;
     }
 }
 
 //copy constructor
 Process::Process(const Process &_proc)
 {
-//set id
-    id = _proc.id;
+  //set id
+  id = _proc.id;
     
-//set lines
-    lines = _proc.lines;
+  //set lines
+  lines = _proc.lines;
 
-//update static member totallinesofcode with the new total of proc lines
-    totalLinesOfCode += lines;
+  //update static member totallinesofcode with the new total of proc lines
+  totalLinesOfCode += lines;
 
-//static member avglinesperprocess with avg of total lines per proc
-    avgLinesPerProcess = totalLinesOfCode / count;
+  //static member avglinesperprocess with avg of total lines per proc
+  avgLinesPerProcess = totalLinesOfCode / count;
 }
 
 //destructor
 Process::~Process()
 {
-    id = 0;
-    lines = 0;
-    totalLinesOfCode = 0;
-    maxLinesOfCode = 0;
-    minLinesOfCode = 0;
-    avgLinesPerProcess = 0;
+  id = 0;
+  lines = 0;
+  totalLinesOfCode = 0;
+  maxLinesOfCode = 0;
+  minLinesOfCode = 0;
+  avgLinesPerProcess = 0;
 }
 
 //setters/mutators
 void Process::set_lines(const int _lines)
 {
-    if (_lines > 0)
+  if (_lines > 0)
     {
-        totalLinesOfCode -= lines; //delete current lines of  proc from total
-        lines = _lines;            //update lines with new _lines
-        totalLinesOfCode += lines; //update total with new _lines
+      totalLinesOfCode -= lines; //delete current lines of  proc from total
+      lines = _lines;            //update lines with new _lines
+      totalLinesOfCode += lines; //update total with new _lines
 
-        //update other lines related static attributes
-        if (_lines > maxLinesOfCode)
+      //update other lines related static attributes
+      if (_lines > maxLinesOfCode)
         {
-            maxLinesOfCode = _lines;
+	  maxLinesOfCode = _lines;
         }
-        else if (_lines < minLinesOfCode)
+      else if (_lines < minLinesOfCode)
         {
-            minLinesOfCode = _lines;
+	  minLinesOfCode = _lines;
         }
     }
 }
@@ -85,50 +85,50 @@ void Process::set_lines(const int _lines)
 //getters
 int Process::get_id() const
 {
-    return id;
+  return id;
 }
 
 int Process::get_lines() const
 {
-    return lines;
+  return lines;
 }
 
 int Process::get_totalLinesOfCode()
 {
-    return totalLinesOfCode;
+  return totalLinesOfCode;
 }
 
 int Process::get_maxLinesOfCode()
 {
-    return maxLinesOfCode;
+  return maxLinesOfCode;
 }
 
 int Process::get_minLinesOfCode()
 {
-    return minLinesOfCode;
+  return minLinesOfCode;
 }
 
 int Process::get_avgLinesPerProcess()
 {
-    return avgLinesPerProcess;
+  return avgLinesPerProcess;
 }
 
 //others
 void Process::display()
 {
-    cout << "\n\tTotal Lines: " << totalLinesOfCode;
-    cout << "\n\tMaximum Lines in a Process: " << maxLinesOfCode;
-    cout << "\n\tMin lines in a process: " << minLinesOfCode;
-    cout << "\n\tAverage Lines per process: " << avgLinesPerProcess;
+  cout << "\n\tTotal Lines: " << totalLinesOfCode;
+  cout << "\n\tMaximum Lines in a Process: " << maxLinesOfCode;
+  cout << "\n\tMin lines in a process: " << minLinesOfCode;
+  cout << "\n\tAverage Lines per process: " << avgLinesPerProcess;
 }
 
 void print(const Process* const* _procs, const int size)
 {
-    cout<<endl;
-    for (int i = 0; i < size; i++)
+  cout<<endl;
+  for (int i = 0; i < size; i++)
     {
-        cout << "\n\tProcess ID: " << _procs[i]->get_id();
-        cout << "\n\tLines: " << _procs[i]->get_lines();
-        cout << endl;
+      cout << "\n\tProcess ID: " << _procs[i]->get_id();
+      cout << "\n\tLines: " << _procs[i]->get_lines();
+      cout << endl;
     }
 }
---
+++ lab9/source.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cstring>
+#include"Process.h"
+#include"Process.cpp"
+using namespace std;
+
+int main(){
+  return 0;
+}
